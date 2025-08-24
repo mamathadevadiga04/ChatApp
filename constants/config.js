146cec5cdcdd573a3fc92d226f2a70eac55c176a
mamathadevadiga04 <mamathadevadiga2001@gmail.com>
2025-08-24 16:08:22 +0530

    config changed

--- constants/config.js
+++ constants/config.js
@@ -1,10 +1,12 @@
-const corsOptions={
-    
-    origin:["http://localhost:5173",
-        "http://localhost:4173",
-        process.env.CLIENT_URL,
-    ],
-    methods:["GET","POST","PUT","DELETE"],
-    credentials:true,
-}
-export {corsOptions}
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:4173",
+    process.env.CLIENT_URL,
+    "https://chat-appfrontend-bice.vercel.app", 
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+export { corsOptions };
